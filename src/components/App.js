7aab8173ab1c168907a78ebcd508148b1ce87c4c
18subrato <subratoverma7@gmail.com>
2025-05-21 19:10:19 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[count,setCount] = useState(0);
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setCount((p)=>p+1);
+    },1000)
+
+    return ()=>{
+      clearInterval(interval);
+    }
+
+  },[count])
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>You've been on this page for {count} seconds</p>
     </div>
   )
 }
